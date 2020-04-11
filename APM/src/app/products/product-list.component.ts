47ada0d4abafc254e672e272969e24a94f9e9759
Simon Headley <headleysj@gmail.com>
2020-04-11 21:51:33 +0200

    Finished observables and http module

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -15,6 +15,7 @@ export class ProductListComponent implements OnInit {
     _listFilter: string;
     filteredProducts: IProduct[];
     products: IProduct[] = [];
+    errorMessage: string;
 
     constructor(private productService: ProductService) {
     }
@@ -30,8 +31,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products,
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        })
     }
 
     peformFilter(filterBy: string): IProduct[] {
